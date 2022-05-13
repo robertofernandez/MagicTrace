1ce81a638e5ac8670d8d3176291008bd49966a25
Roberto G. Fernández <rgfernan@gmail.com>
2022-05-13 20:30:38 -0300

    Added multiple trace contrasts.

--- src/test/java/studio/itpex/magictrace/tests/ide/MagicTraceTestIde3.java
+++ src/test/java/studio/itpex/magictrace/tests/ide/MagicTraceTestIde3.java
@@ -40,11 +40,12 @@ public class MagicTraceTestIde3 extends JFrame {
     public static final int desktopHeight = 680;
     private JDesktopPane mainDesktopPane;
     private static final long serialVersionUID = 6733330978640433656L;
-    private ImagePanel cameraPanel;
-    private ImagePanel tracePanel;
+    private ImagePanel panel0;
+    private ImagePanel panel1;
     private static double proportion = 0.8;
 
     private ImagePanel[] allViews = new ImagePanel[2];
+    private String[] currentViews = new String[2];
     private int currentTargetedView = 0;
 
     private JMenuBar menuBar;
@@ -62,13 +63,13 @@ public class MagicTraceTestIde3 extends JFrame {
     protected static void createAndShowGUI() {
         CurrentMagicTraceTestIdeMaps currentMaps = new CurrentMagicTraceTestIdeMaps();
         MagicTraceTestIde3 ide = new MagicTraceTestIde3();
-        
+
         ide.pack();
         ide.setVisible(true);
         new Thread(new Runnable() {
             public void run() {
                 try {
-                    
+
                     ide.initialize();
                     String log4jConfPath = "src/main/resources/config/log4j.properties";
                     PropertyConfigurator.configure(log4jConfPath);
@@ -116,7 +117,7 @@ public class MagicTraceTestIde3 extends JFrame {
 
                     // for (int i = 0; i < 190; i++) {
                     for (;;) {
-                        //System.out.println("sleeping " + i);
+                        // System.out.println("sleeping " + i);
                         Thread.sleep(FRAME_TIME);
                         if (teemoImageMap != null && SHOW_TEEMO) {
                             updateImageFromCameraMixing(ide, webcam, teemoImageMap, proportion, currentMaps);
@@ -138,6 +139,7 @@ public class MagicTraceTestIde3 extends JFrame {
         viewsMenu = createViewsMenu();
         menuBar.add(operationsMenu);
         menuBar.add(viewsMenu);
+        menuBar.add(createProportionsMenu());
         setJMenuBar(menuBar);
     }
 
@@ -153,7 +155,7 @@ public class MagicTraceTestIde3 extends JFrame {
         });
         return menu;
     }
-    
+
     private JMenu createViewsMenu() {
         JMenu menu = new JMenu("Views");
         addViewMenuItem(menu, CAMERA_VIEW);
@@ -162,6 +164,25 @@ public class MagicTraceTestIde3 extends JFrame {
         addViewMenuItem(menu, MAGIC_PAINT_TRACE_VIEW);
         addViewMenuItem(menu, CONTRAST_VIEW);
         return menu;
+    }
+
+    private JMenu createProportionsMenu() {
+        JMenu menu = new JMenu("Trace contrast");
+        for (int i = 10; i < 110; i+=10) {
+            addProportionMenuItem(menu, i);
+        }
+        return menu;
+    }
+
+    private void addProportionMenuItem(JMenu menu, int proportionPercentage) {
+        JMenuItem cameraView = new JMenuItem("" + proportionPercentage + "%");
+        menu.add(cameraView);
+        cameraView.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                proportion = ((double) proportionPercentage) / 100;
+            }
+        });
     }
 
     private void addViewMenuItem(JMenu menu, String title) {
@@ -177,6 +198,7 @@ public class MagicTraceTestIde3 extends JFrame {
 
     private void setView(String title) {
         // FIXME implement
+
         trace("info", "View set to " + title);
     }
 
@@ -185,58 +207,62 @@ public class MagicTraceTestIde3 extends JFrame {
         mainDesktopPane = new JDesktopPane();
         mainDesktopPane.setPreferredSize(new Dimension(desktopWidth, desktopHeight));
 
-        cameraPanel = new ImagePanel("Combined Area");
-        mainDesktopPane.add(cameraPanel);
-        cameraPanel.setLocation(0, 0);
-        tracePanel = new ImagePanel("Trace Area");
-        mainDesktopPane.add(tracePanel);
-        tracePanel.setLocation(650, 0);
-        
-        allViews[0] = cameraPanel;
-        allViews[1] = tracePanel;
-        
-        tracePanel.setTitle("Trace Area");
-        
-        tracePanel.setFocusable(false);
-        cameraPanel.setFocusable(false);
-        //cameraPanel.setDecoration(5);
+        panel0 = new ImagePanel("Panel 0");
+        mainDesktopPane.add(panel0);
+        panel0.setLocation(0, 0);
+        panel1 = new ImagePanel("Panel 1");
+        mainDesktopPane.add(panel1);
+        panel1.setLocation(650, 0);
+
+        allViews[0] = panel0;
+        allViews[1] = panel1;
+
+        currentViews[0] = TRANSPARENCY_TRACE_VIEW;
+        currentViews[1] = CAMERA_VIEW;
+
+        panel0.setTitle(TRANSPARENCY_TRACE_VIEW);
+        panel1.setTitle(CAMERA_VIEW);
+
+        panel1.setFocusable(false);
+        panel0.setFocusable(false);
+        // cameraPanel.setDecoration(5);
 
         this.getContentPane().add("North", mainDesktopPane);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(MAXIMIZED_BOTH);
-        
+
         KeyAdapter keyAdapter = new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                //System.out.println("typed " + e.getKeyChar());
+                // System.out.println("typed " + e.getKeyChar());
             }
-            
+
             @Override
             public void keyReleased(KeyEvent e) {
-                //System.out.println("released " + e.getKeyChar());
+                // System.out.println("released " + e.getKeyChar());
 
             }
-            
+
             @Override
             public void keyPressed(KeyEvent e) {
-                //System.out.println("pressed " + e.getKeyChar());
+                // System.out.println("pressed " + e.getKeyChar());
             }
         };
-        
+
         addKeyListener(keyAdapter);
     }
 
     public ImagePanel getCameraPanel() {
-        return cameraPanel;
+        return panel0;
     }
 
     public ImagePanel getTracePanel() {
-        return tracePanel;
+        return panel1;
     }
 
     private static void updateImageFromCamera(MagicTraceTestIde3 ide, Webcam webcam) throws Exception {
         BufferedImage image = webcam.getImage();
-        //System.out.println("Image retrieved");
+        // System.out.println("Image retrieved");
         ImageRepresentation representation = new ImageRepresentation(image);
         ColorMap imageMap = new ColorMap(image.getWidth(), image.getHeight());
         imageMap.setRed(representation.getRed());
@@ -248,7 +274,7 @@ public class MagicTraceTestIde3 extends JFrame {
 
         ide.getCameraPanel().setMap(enlargedMap);
         // ide.getCameraPanel().updateImage();
-        //System.out.println("Image updated");
+        // System.out.println("Image updated");
     }
 
     private static void updateImageFromCurrentMap(ImagePanel panel, ColorMap currentMap) throws Exception {
@@ -265,7 +291,7 @@ public class MagicTraceTestIde3 extends JFrame {
     private static void updateImageFromCameraMixing(MagicTraceTestIde3 ide, Webcam webcam, ColorMap mixingMap,
             double proportion, CurrentMagicTraceTestIdeMaps currentMaps) throws Exception {
         BufferedImage lastCameraImage = webcam.getImage();
-        //System.out.println("Image retrieved");
+        // System.out.println("Image retrieved");
         ImageRepresentation representation = new ImageRepresentation(lastCameraImage);
         ColorMap imageMap = new ColorMap(lastCameraImage.getWidth(), lastCameraImage.getHeight());
         imageMap.setRed(representation.getRed());
@@ -281,9 +307,9 @@ public class MagicTraceTestIde3 extends JFrame {
 
         ide.getCameraPanel().setMap(mixedMap);
         // ide.getCameraPanel().updateImage();
-        //System.out.println("Image updated");
+        // System.out.println("Image updated");
     }
-    
+
     private void setBaseSheet() {
         // FIXME implement set base sheet using current camera view
         trace("info", "Base sheet stored");
@@ -291,8 +317,6 @@ public class MagicTraceTestIde3 extends JFrame {
 
     private void trace(String level, String message) {
         System.out.println("[" + level + "] " + message);
-        
     }
-
 
 }
